3be23147f5cb628aa907ea06ab125562b7651980
Dinesh <dineshkumar.software@gmail.com>
2023-06-03 13:09:44 +0530

    Implemented First ML Model

--- Assets/Scripts/PlayerAgent.cs
+++ Assets/Scripts/PlayerAgent.cs
@@ -8,7 +8,17 @@ using Unity.MLAgents.Sensors;
 public class PlayerAgent : Agent
 {
 
+    [SerializeField] private Vector3 initalPos;
     [SerializeField] private Transform targetPos;
+
+    private float moveSpeed=2f;
+
+    private float previousDistance;
+
+    public override void OnEpisodeBegin()
+    {
+        transform.position = initalPos;
+    }
 
     public override void CollectObservations(VectorSensor sensor)
     {
@@ -17,9 +27,44 @@ public class PlayerAgent : Agent
     }
     public override void OnActionReceived(ActionBuffers actions)
     {
-        Debug.Log(actions.DiscreteActions[0]);
-        Debug.Log(actions.DiscreteActions[1]);
+        float vertical = actions.ContinuousActions[0] * Time.deltaTime * moveSpeed;
+        float horizontal = actions.ContinuousActions[1] * Time.deltaTime * moveSpeed;
 
-        
+        transform.Translate(vertical, 0, horizontal);
+    }
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.name == "Goal")
+        {
+            SetReward(1f);
+            EndEpisode();
+        }
+        if (collision.gameObject.tag == "Wall")
+        {
+            SetReward(-2f);
+            EndEpisode();
+        }
+    }
+
+
+
+    private void Update()
+    {
+        previousDistance = Vector3.Distance(gameObject.transform.position, targetPos.position);
+    }
+
+    private void LateUpdate()
+    {
+        float difference = previousDistance - Vector3.Distance(gameObject.transform.position, targetPos.position);
+
+        if (difference >= 0)
+        {
+            AddReward(.5f);
+        }
+        else
+        {
+            AddReward(-.5f);
+        }
     }
 }
