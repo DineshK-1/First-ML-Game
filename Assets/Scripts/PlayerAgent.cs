c4655573fabf101fc08ac6d314f5544654534d4a
Dinesh <dineshkumar.software@gmail.com>
2023-06-03 14:16:39 +0530

    Revised parameters and added distance to target

--- Assets/Scripts/PlayerAgent.cs
+++ Assets/Scripts/PlayerAgent.cs
@@ -8,22 +8,31 @@ using Unity.MLAgents.Sensors;
 public class PlayerAgent : Agent
 {
 
+    [SerializeField] private GameObject target;
+
     [SerializeField] private Vector3 initalPos;
     [SerializeField] private Transform targetPos;
 
-    private float moveSpeed=2f;
+    [SerializeField] private Renderer planeMat;
+
+    [SerializeField] private Material success;
+    [SerializeField] private Material failure;
+
+    private float moveSpeed=.5f;
 
     private float previousDistance;
 
     public override void OnEpisodeBegin()
     {
-        transform.position = initalPos;
+        transform.localPosition = new Vector3(Random.Range(-8.6f, 19.35f), 15.30216f, Random.Range(-3.82f, 22.5f));
+
+        target.transform.localPosition = new Vector3(Random.Range(-8.6f, 19.35f), 15.30216f, Random.Range(-3.82f, 22.5f));
     }
 
     public override void CollectObservations(VectorSensor sensor)
     {
-        sensor.AddObservation(transform.position);
-        sensor.AddObservation(targetPos.position);
+        sensor.AddObservation(transform.localPosition);
+        sensor.AddObservation(targetPos.localPosition);
     }
     public override void OnActionReceived(ActionBuffers actions)
     {
@@ -38,33 +47,35 @@ public class PlayerAgent : Agent
         if(collision.gameObject.name == "Goal")
         {
             SetReward(1f);
+            planeMat.material = success;
             EndEpisode();
         }
         if (collision.gameObject.tag == "Wall")
         {
-            SetReward(-2f);
+            SetReward(-1f);
+            planeMat.material = failure;
             EndEpisode();
         }
     }
 
-
-
     private void Update()
     {
-        previousDistance = Vector3.Distance(gameObject.transform.position, targetPos.position);
+        previousDistance = Vector3.Distance(gameObject.transform.localPosition, targetPos.localPosition);
     }
 
     private void LateUpdate()
     {
-        float difference = previousDistance - Vector3.Distance(gameObject.transform.position, targetPos.position);
+        float difference = previousDistance - Vector3.Distance(gameObject.transform.localPosition, targetPos.localPosition);
 
-        if (difference >= 0)
+        if (difference > 0f)
         {
-            AddReward(.5f);
+            SetReward(.5f);
+            planeMat.material = success;
         }
-        else
+        if (difference < 0f)
         {
-            AddReward(-.5f);
+            SetReward(-.5f);
+            planeMat.material = failure;
         }
     }
 }
